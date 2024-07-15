a4a1fc9b22e36fcd6a314153f4c8e8f1b8defe6e
Nedal <nedalosama3@gmail.com>
2024-07-15 22:53:05 +0300

    nasted Binding

---
+++ Assignment3/Binding/EMP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Binding
+{
+    internal class EMP
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int? age { get; set; }
+
+        public void fun1()
+        {
+            Console.WriteLine("I am Emplyee");
+        }
+        public virtual void fun2()
+        {
+            Console.WriteLine($"id:{id},name:{name},age:{age}");
+        }
+    }
+}
--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -1,14 +1,23 @@
-﻿using Assignment3.Overriding;
+﻿using Assignment3.Binding;
+using Assignment3.Overriding;
 
 namespace Assignment3
 {
+   
     internal class Program
     {
         private static object typeB;
-
+        public static void processEmp(FullTimeEmp emp)
+        {
+            if (emp != null)
+            {
+                emp.fun1();
+                emp.fun2();
+            }
+        }
         static void Main(string[] args)
         {
-         /*  #region Overriding
+          #region Overriding
             TypeA typeA = new TypeA(1);
             typeA.A = 2;
 
@@ -19,7 +28,8 @@ namespace Assignment3
             typeB.A = 3;
             typeB.fun1();
             typeB.fun2();
-            #endregion*/
+            #endregion
+
 
             #region Binding 
             TypeA RefBase = new TypeB(1, 2);
@@ -28,6 +38,17 @@ namespace Assignment3
             RefBase.fun1();
             RefBase.fun2();
             #endregion
+
+            // TypeB typeB=(TypeB)typeA;      unsaif 
+
+            #region nasted
+            FullTimeEmp emp = new FullTimeEmp();
+            emp.id = 10;
+            emp.name = "Nedal";
+            emp.age = 22;
+            emp.salary = 50000;
+            #endregion
+
         }
     }
 }
