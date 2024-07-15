db9523c234b67d282d54d54d8839391b14b3ac3a
Nedal <nedalosama3@gmail.com>
2024-07-15 22:08:20 +0300

    Add project files.

---
+++ Assignment3/Overriding/TypeA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Overriding
+{
+    public class TypeA
+    {
+        public int A { get; set; } 
+        public TypeA (int _a)
+        {
+            A= _a;
+        }
+        public   void fun1()
+        {
+            Console.WriteLine("I am BAse");
+        }
+        public void fun2()
+        {
+            Console.WriteLine($"Type A :{A}");
+        }
+
+    }
+    public class TypeB:TypeA
+    {
+        public int B { get; set; }
+        public TypeB(int _a ,int _b):base(_a)
+        {
+            B = _b;
+        }
+        public new void fun1()
+        {
+            Console.WriteLine("I am BAse");
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,33 @@
+﻿using Assignment3.Overriding;
+
+namespace Assignment3
+{
+    internal class Program
+    {
+        private static object typeB;
+
+        static void Main(string[] args)
+        {
+            #region Overriding
+            TypeA typeA = new TypeA(1);
+            typeA.A = 2;
+
+            typeA.fun1();
+            typeA.fun2();
+
+            TypeB typeB = new TypeB(1,2);
+            typeB.A = 3;
+            typeB.fun1();
+            typeB.fun2();
+            #endregion
+            #region Binding
+
+            TypeA RefBase=new TypeB(1,2);
+            RefBase.A = 3;
+         //   RefBase.B= 4;  invalid
+            RefBase.fun1();
+            RefBase.fun2();
+            #endregion
+        }
+    }
+}
